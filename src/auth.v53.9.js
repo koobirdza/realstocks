9fd746334ca998debf3cbe8f142d7061d55c88c1
koobirdza <koobirdza@gmail.com>
2026-05-01 13:41:27 +0700

    Delete src directory

--- src/auth.v53.9.js
+++
@@ -1,5 +0,0 @@
-import { getSession, setSession, clearSessionStore } from "./store.v53.9.js";
-import { setEmployee } from "./state.v53.9.js";
-export function restoreSession() { const session = getSession(); if (session?.employee) setEmployee(session.employee); }
-export function persistSession(employee) { setSession(employee); }
-export function logoutSession() { clearSessionStore(); setEmployee(""); }
